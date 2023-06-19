fd40942e70e74cd708ba232a04993b8fe4e1060c
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-06-19 16:04:15 -0400

    :D

--- src/Componentes/ListaDeTareas.jsx
+++ src/Componentes/ListaDeTareas.jsx
@@ -10,21 +10,33 @@ const ListaDeTareas = () => {
       tarea.texto = tarea.texto.trim();
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas);
-    }
+    }else return alert("Por favor introduce una tarea");
   };
 
   return (
     <>
       <TareaFormulario onSubmit={agregarTarea} />
       <div className="tareas-lista-contenedor">
-        {tareas.map((tarea) => {
+        {/* FORMA 1  */}
+        {/* {tareas.map((tarea) => {
+          return (
+            <Tarea
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada}
+            />
+          );
+        })} */}
+        {/* FORMA 2 */}
+        {tareas.map(({id, texto, completada}) => (
           <Tarea
-            key={tarea.id}
-            id={tarea.id}
-            texto={tarea.texto}
-            completada={tarea.completada}
-          />;
-        })}
+            key={id}
+            id={id}
+            texto={texto}
+            completada={completada}
+          />
+        ))}
       </div>
     </>
   );
